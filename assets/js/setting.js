b458bb4f5b7cfe33a0bca5840be750fe767842b2
alfurqanpaser <webmasteralfurqan@gmail.com>
2024-01-10 08:15:18 +0800

    Update Countdown

--- assets/js/setting.js
+++ assets/js/setting.js
@@ -1,10 +1,10 @@
 simplyCountdown(".simply-countdown", {
-    year: 2023, // required
-    month: 12, // required
-    day: 31, // required
-    hours: 23, // Default is 0 [0-23] integer
-    minutes: 59, // Default is 0 [0-59] integer
-    seconds: 59, // Default is 0 [0-59] integer
+    year: 2024, // required
+    month: 2, // required
+    day: 1, // required
+    hours: 0, // Default is 0 [0-23] integer
+    minutes: 0, // Default is 0 [0-59] integer
+    seconds: 0, // Default is 0 [0-59] integer
     words: {
         //words displayed into the countdown
         days: { singular: "Hari", plural: "Hari" },
