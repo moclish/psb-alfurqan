188421443586c3a6d2a0b0a0d1f704d4091ba23b
alfurqanpaser <webmasteralfurqan@gmail.com>
2024-02-01 07:54:37 +0800

    Start Gel 2

--- assets/js/setting.js
+++ assets/js/setting.js
@@ -1,10 +1,10 @@
 simplyCountdown(".simply-countdown", {
     year: 2024, // required
     month: 2, // required
-    day: 1, // required
-    hours: 0, // Default is 0 [0-23] integer
-    minutes: 0, // Default is 0 [0-59] integer
-    seconds: 0, // Default is 0 [0-59] integer
+    day: 15, // required
+    hours: 23, // Default is 0 [0-23] integer
+    minutes: 59, // Default is 0 [0-59] integer
+    seconds: 59, // Default is 0 [0-59] integer
     words: {
         //words displayed into the countdown
         days: { singular: "Hari", plural: "Hari" },
